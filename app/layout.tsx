106b11ce67a9052376a6a55e46feeb8ea1d71389
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-06-11 09:22:58 +0800

    Implement NextAuth Authentication with Firebase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,46 @@
+'use client';
+
 import SideBar from "@/components/sidebar";
 import "../app/globals.css";
+import { SessionProvider } from "next-auth/react";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+import Login from "@/components/Login";
 
 export const metadata = {
   title: 'ChatGPT Clone',
   description: 'A ChatGPT Clone generated using Next.js',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
+  const session = await getServerSession(authOptions)
+
   return (
     <html lang="en">
       <head />
       <body>
-        <div className="flex">
-          <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
-            <SideBar />
-          </div>
+        <SessionProvider session={session}>
+          {!session ? (
+            <Login />
+          ): (
+            <div className="flex">
+              <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
+                <SideBar />
+              </div>
 
-          {/* ClientProvider - Notification */}
-          
+              {/* ClientProvider - Notification */}
+              
 
 
-          <div className="bg-[#343541] flex-1">{children}</div>
-        </div>
+              <div className="bg-[#343541] flex-1">{children}</div>
+            </div>
+          )}
+        </SessionProvider>
       </body>
     </html>
   )
