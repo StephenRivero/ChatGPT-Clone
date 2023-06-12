f733b3af1968ffb7d21ff0c7334f2d319fd81f52
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-06-12 09:57:37 +0800

    Add Delete Chat Functionality

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -1,6 +1,6 @@
 import { db } from "@/firebase";
 import { ChatBubbleLeftIcon, TrashIcon } from "@heroicons/react/24/outline";
-import { collection, orderBy, query } from "firebase/firestore";
+import { collection, deleteDoc, doc, orderBy, query } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
@@ -18,10 +18,7 @@ function ChatRow({ id }: Props) {
     const [active, setActive] = useState(false);
 
     const [messages] = useCollection(
-        query(
-            collection(db, 'users', session?.user?.email!, 'chats', id, 'messages'),
-            orderBy('createdAt', "asc")
-        )
+        collection(db, 'users', session?.user?.email!,'chats', id, 'messages'),
     );
 
     useEffect(() => {
@@ -29,13 +26,18 @@ function ChatRow({ id }: Props) {
         setActive(pathname.includes(id));
     }, [pathname]);
 
+    const removeChat = async() => {
+        await deleteDoc(doc(db, 'users', session?.user?.email!, 'chats', id));
+        router.replace("/");
+    };
+
   return (
     <Link href={`/chat/${id}`} className={`chatRow  justify-center ${active && "bg-gray-700/50"}`}>
         <ChatBubbleLeftIcon className="h-5 w-5" />
         <p className="flex-1 hidden md:inline-flex truncate">
             {messages?.docs[messages?.docs.length - 1]?.data().text || "New Chat"}
         </p>
-        <TrashIcon className="h-5 w-5 text-gray-700 hover:text-red-700" />
+        <TrashIcon onClick={removeChat} className="h-5 w-5 text-gray-700 hover:text-red-700" />
 
        
     </Link>
