b423d09776c012f4854c35cbd1c6438b885add65
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-06-15 20:49:27 +0800

    React Hot Toast Library for Notifications

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -5,6 +5,7 @@ import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { FormEvent, useState } from "react";
+import { toast } from "react-hot-toast";
 
 type Props = {
     chatId: string;
@@ -13,6 +14,10 @@ type Props = {
 function ChatInput({ chatId }: Props) {
     const [prompt, setPrompt] = useState("");
     const { data: session } = useSession();
+
+    // useSWR to get model
+    const model = "text-davinci-003"
+
     const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         if (!prompt) return;
@@ -33,6 +38,28 @@ function ChatInput({ chatId }: Props) {
             collection(db, 'users', session?.user?.email!, 'chats', chatId, 'messages'), 
             message
         )
+
+        // Toast notification to say Loading!
+        const notification = toast.loading('ChatGPT is thinking...');
+
+
+        await fetch('/api/askQuestion', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                prompt: input,
+                chatId,
+                model,
+                session,
+            }),
+        }).then(() => {
+            // Toast notification to say successful!
+            toast.success('ChatGPT has responded!', {
+                id: notification,
+            });
+        });
     };
     return (
         <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm  ">
